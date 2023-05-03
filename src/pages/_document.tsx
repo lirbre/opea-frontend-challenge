eceb40dc384514a3779c16a911142b9612844943
Breno Lira <brenoliradev@gmail.com>
2023-05-03 20:24:13 -0300

    feat: implement opea logos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="https://www.opeacapital.com/wp-content/uploads/2022/06/cropped-favicon-1-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          href="https://www.opeacapital.com/wp-content/uploads/2022/06/cropped-favicon-1-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://www.opeacapital.com/wp-content/uploads/2022/06/cropped-favicon-1-180x180.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
