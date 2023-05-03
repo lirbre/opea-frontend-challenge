c981fbb80815a23e304aae4dc8126bdc11a488b3
Breno Lira <brenoliradev@gmail.com>
2023-05-03 20:47:55 -0300

    feat: modal footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
       colors: {
         'white-brand': '#f7f7f7',
         'gray-brand': '#959799',
-        'gray-text': 'rgba(0, 0, 0, 0.3)'
+        'gray-text': 'rgba(0, 0, 0, 0.3)',
+        'gray-input': '#DDDDDD',
+        'gray-button': '#C7C9CC',
+        'wine-brand': '#630A37',
+        'gray-helper': '#797979'
+      },
+      borderRadius: {
+        opea: '4px'
       }
     }
   },
