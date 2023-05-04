88278e75c4fbfc3032ff82e98cba0cceb817d201
Breno Lira <brenoliradev@gmail.com>
2023-05-04 09:54:03 -0300

    feat: company create component

---
+++ src/components/CreateCompany.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+export const CreateCompany = () => {
+  const { query, asPath, replace } = useRouter()
+
+  return (
+    <button
+      aria-label="Create company button"
+      className="flex w-full items-center justify-center rounded-l-full border-2 border-gray-input p-2 hover:opacity-80"
+      onClick={() =>
+        replace({ href: asPath, query: { ...query, create: true } })
+      }
+    >
+      <div className="mr-auto flex items-center justify-center rounded-full border-2 border-gray-input px-3 py-2.5">
+        <Image
+          src="/images/clipboard-list-icn.svg"
+          width={14}
+          height={20}
+          alt="A gray clipboard icon"
+        />
+      </div>
+      <p className="mr-auto text-gray-brand">Adicionar empresa</p>
+    </button>
+  )
+}
