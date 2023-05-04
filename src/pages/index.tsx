acf2806cfd6840f4e0cf712ebe2e701446a8b0b8
Breno Lira <brenoliradev@gmail.com>
2023-05-03 21:26:04 -0300

    feat: title/meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image'
 import { Jost } from 'next/font/google'
+import Head from 'next/head'
 
 const jost = Jost({ subsets: ['latin'] })
 
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <Head>
+        <title>Frontend Challenge - Opea</title>
+        <meta name="description" content="A CRUD for Opea Frontend Challenge" />
+      </Head>
+    </main>
+  )
 }
