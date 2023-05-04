f7e89821621f352efd675ae90223ae3ebd1e7054
Breno Lira <brenoliradev@gmail.com>
2023-05-04 09:53:46 -0300

    feat: company searchbar controller

---
+++ src/components/CompanyControls.tsx
@@ -0,0 +1,9 @@
+import { SearchBar } from './SearchBar'
+
+export const CompanyControls = () => {
+  return (
+    <div className="flex w-full items-center justify-end px-8 pt-8">
+      <SearchBar />
+    </div>
+  )
+}
