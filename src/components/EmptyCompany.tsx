ec51e262e1f949f76c3cc3f965b40995d29c2edc
Breno Lira <brenoliradev@gmail.com>
2023-05-04 09:54:11 -0300

    feat: company empty component

---
+++ src/components/EmptyCompany.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export const EmptyCompany = () => {
+  return (
+    <div className="flex w-full items-center gap-6 rounded-l-full border-2 border-transparent bg-white p-2 shadow-sm">
+      <div className="flex items-center justify-center rounded-full border-2 border-gray-input px-3 py-2.5">
+        <Image
+          src="/images/clipboard-list-icn.svg"
+          width={14}
+          height={20}
+          alt="A gray clipboard icon"
+        />
+      </div>
+      <p className="text-left text-gray-font">
+        Não foi possivel encontrar nenhuma empresa.
+      </p>
+    </div>
+  )
+}
