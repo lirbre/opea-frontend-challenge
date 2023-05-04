875735dc6e5b89f340cc1d233f5dbdf17230daf7
Breno Lira <brenoliradev@gmail.com>
2023-05-04 15:37:56 -0300

    fix: add a gap between controls

--- src/components/CompanyControls.tsx
+++ src/components/CompanyControls.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from './SearchBar'
 
 export const CompanyControls = () => {
   return (
-    <div className="flex w-full items-center justify-between px-8 pt-8">
+    <div className="flex w-full items-center justify-between px-8 pt-8 gap-4">
       <CompanyPagination />
       <SearchBar />
     </div>
