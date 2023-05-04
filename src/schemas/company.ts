9cd69970dad4793a01ef0589ea7ee7f12a426dcf
Breno Lira <brenoliradev@gmail.com>
2023-05-03 23:01:06 -0300

    fix: typo on schema

--- src/schemas/company.ts
+++ src/schemas/company.ts
@@ -3,15 +3,16 @@ import { z } from 'zod'
 export const RegexCNPJ =
   /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/
 
-export const CompayForm = z.object({
+export const CompanyForm = z.object({
   name: z.string().min(1, { message: 'Por favor, insira um nome.' }),
   email: z.string().min(1, { message: 'Por favor, insira um email válido.' }),
   cnpj: z.string().length(14, { message: 'Por favor, insira um CNPJ válido.' })
 })
 
-export const CompayAPI = z.object({
+export const CompanyAPI = z.array(z.object({
   id: z.string(),
   name: z.string(),
   email: z.string(),
   cnpj: z.string()
 })
+)
