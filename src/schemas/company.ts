c6e0578425fc119af5aa1b09162260a4b723d7e9
Breno Lira <brenoliradev@gmail.com>
2023-05-04 09:52:59 -0300

    feat: cnpj should be always numeric

--- src/schemas/company.ts
+++ src/schemas/company.ts
@@ -1,18 +1,23 @@
 import { z } from 'zod'
 
-export const RegexCNPJ =
-  /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/
+export const RegexCNPJ = /[0-9]{14}/
 
 export const CompanyForm = z.object({
   name: z.string().min(1, { message: 'Por favor, insira um nome.' }),
   email: z.string().min(1, { message: 'Por favor, insira um email válido.' }),
-  cnpj: z.string().length(14, { message: 'Por favor, insira um CNPJ válido.' })
+  cnpj: z
+    .string()
+    .length(14, { message: 'Por favor, insira um CNPJ válido.' })
+    .regex(RegexCNPJ, {
+      message: 'Por favor, insira somente os digitos numéricos.'
+    })
 })
 
-export const CompanyAPI = z.array(z.object({
-  id: z.string(),
-  name: z.string(),
-  email: z.string(),
-  cnpj: z.string()
-})
+export const CompanyAPI = z.array(
+  z.object({
+    id: z.string(),
+    name: z.string(),
+    email: z.string(),
+    cnpj: z.string()
+  })
 )
