4b62971f1766693094717e2496cec0144b4252d1
Breno Lira <brenoliradev@gmail.com>
2023-05-05 10:24:27 -0300

    feat: logos to /public instead of picking from opea website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,20 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/cropped-favicon-1-32x32.png" sizes="32x32" />
         <link
           rel="icon"
-          href="https://www.opeacapital.com/wp-content/uploads/2022/06/cropped-favicon-1-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          href="https://www.opeacapital.com/wp-content/uploads/2022/06/cropped-favicon-1-192x192.png"
+          href="/cropped-favicon-1-192x192.png"
           sizes="192x192"
         />
-        <link
-          rel="apple-touch-icon"
-          href="https://www.opeacapital.com/wp-content/uploads/2022/06/cropped-favicon-1-180x180.png"
-        />
+        <link rel="apple-touch-icon" href="/cropped-favicon-1-180x180.png" />
       </Head>
       <body>
         <Main />
